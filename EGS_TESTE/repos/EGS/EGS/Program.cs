8cbb273a5f94105631fa90aefe0cf2979fafea49
William Agostinho <williamagostinho@gmail.com>
2020-07-31 22:39:03 -0300

    exercico sql

---
+++ EGS_TESTE/repos/EGS/EGS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace EGS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor, result,desconto ;
+            
+            Console.WriteLine("Informe Aqui o Valor do Produto ");
+            Console.WriteLine("--------------------------------");
+            valor = (double)Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("O Produto Custava:" + valor);
+            
+            result = valor * 0.60;
+            Console.WriteLine("Com  Desconto: " + result);
+
+            desconto = valor - result;
+            
+            Console.WriteLine("Valor do descobto: " + desconto);
+           
+            Console.ReadKey();
+
+        }
+    }
+}
